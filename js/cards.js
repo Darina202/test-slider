3e5f17409f58796a78ea35c50fb3f7a965a41b21
Darina202 <138057834+Darina202@users.noreply.github.com>
2023-11-21 21:42:58 +0200

    update

---
+++ js/cards.js
@@ -0,0 +1,42 @@
+export const cardItems = [
+  {
+    code: 'Kod: 00000001',
+    picture:
+      'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg',
+    title: 'Anime 1',
+    priceNow: '5000 ₴',
+    priceBefore: '6100 ₴',
+  },
+  {
+    code: 'Kod: 00000002',
+    picture:
+      'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg',
+    title: 'Anime 2',
+    priceNow: '5200 ₴',
+    priceBefore: '6200 ₴',
+  },
+  {
+    code: 'Kod: 00000003',
+    picture:
+      'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg',
+    title: 'Anime 3',
+    priceNow: '3000 ₴',
+    priceBefore: '3100 ₴',
+  },
+  {
+    code: 'Kod: 00000004',
+    picture:
+      'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg',
+    title: 'Anime 4',
+    priceNow: '4000 ₴',
+    priceBefore: '4100 ₴',
+  },
+  {
+    code: 'Kod: 00000005',
+    picture:
+      'https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg',
+    title: 'Anime 5',
+    priceNow: '5500 ₴',
+    priceBefore: '6100 ₴',
+  },
+];
--- js/script2.js
+++ js/script2.js
@@ -1,6 +1,7 @@
-const swiper = new Swiper(".swiper", {
+import { cardItems } from './cards';
+
+const swiper = new Swiper('.swiper', {
   // Optional parameters
-  //   direction: "horizontal",
   loop: true,
   slidesPerView: 3,
   spaceBetween: 20,
@@ -11,7 +12,52 @@ const swiper = new Swiper(".swiper", {
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
+
+const myList = document.querySelector('.slider-list');
+
+// const array = [
+//   {
+//     code: 'Код: 00000001',
+//     title: 'Аниме 1',
+//   },
+//   { code: 'Код: 00000002', title: 'Аниме 2' },
+//   { code: 'Код: 00000003', title: 'Аниме 3' },
+// ];
+// function createCards(arr) {
+//   return arr
+//     .map(
+//       ({ code, title }) => `
+//          <li class="slider-item">
+//                 <h3 class="slider-code">${code}</h3>
+//                            <p class="title-slider">${title}</p>
+//               </li>
+//   `
+//     )
+//     .join('');
+// }
+myList.insertAdjacentHTML('beforeend', createCards(cardItems));
+
+function createCards(arr) {
+  return arr
+    .map(
+      ({ code, picture, title, priceNow, priceBefore }) => `
+       <div class="slider-item swiper-slide">
+              <h3 class="slider-code">${code}</h3>
+              // <img class="box-img" src=${picture} alt="" width="360" />
+
+              <p class="title-slider">${title}</p>
+
+              <div class="price-offer">
+                <p class="price-now-offer">${priceNow}</p>
+                <p class="price-before-offer">${priceBefore}</p>
+              </div>
+              <button type="submit" class="menu-button-offer">"Замовити"</button>
+            </div>
+`
+    )
+    .join('');
+}
